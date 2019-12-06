8500fbd70a691855bbc41292f1e0194ea64778b4
panossot <psotirop@redhat.com>
2019-12-06 10:22:38 +0100

    Minor update ...

--- modules/src/test/java/io/vertx/core/http/Http1xTest.java
+++ modules/src/test/java/io/vertx/core/http/Http1xTest.java
@@ -42,6 +42,7 @@ import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.LongConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -2065,13 +2066,17 @@ public class Http1xTest extends HttpTest {
     await();
   }
 
-  private void fill(Buffer buffer, WriteStream<Buffer> ws, Runnable done) {
+  private void fill(Buffer buffer, WriteStream<Buffer> ws, LongConsumer done) {
+    fill(0, buffer, ws, done);
+  }
+
+  private void fill(long amount, Buffer buffer, WriteStream<Buffer> ws, LongConsumer done) {
     if (ws.writeQueueFull()) {
-      done.run();
+      done.accept(amount);
     } else {
       ws.write(buffer);
-      vertx.runOnContext(v -> {
-        fill(buffer, ws, done);
+      vertx.setTimer(1, v -> {
+        fill(amount + buffer.length(), buffer, ws, done);
       });
     }
   }
@@ -2085,38 +2090,39 @@ public class Http1xTest extends HttpTest {
     Context serverCtx = vertx.getOrCreateContext();
     CountDownLatch latch = new CountDownLatch(1);
     List<HttpServerRequest> requests = Collections.synchronizedList(new ArrayList<>());
-    Map<String, CompletionStage<Void>> requestResumeMap = new HashMap<>();
+    Map<String, CompletionStage<Long>> requestResumeMap = new HashMap<>();
     Map<String, CompletionStage<Void>> responseResumeMap = new HashMap<>();
     serverCtx.runOnContext(v1 -> {
       server.requestHandler(req -> {
         req.pause();
-        requestResumeMap.get(req.path()).thenAccept(v -> {
-          req.resume();
-        });
         assertSameEventLoop(serverCtx, Vertx.currentContext());
         Buffer body = Buffer.buffer();
+        requestResumeMap.get(req.path()).thenAccept(amount -> {
+          req.resume();
+          req.endHandler(v2 -> {
+            assertSameEventLoop(serverCtx, Vertx.currentContext());
+            assertEquals((long)amount, body.length());
+            requests.add(req);
+            if (requests.size() == numReqs) {
+              requests.forEach(req_ ->{
+                HttpServerResponse resp = req_.response();
+                CompletableFuture<Void> cf = new CompletableFuture<>();
+                responseResumeMap.put(req_.path(), cf);
+                resp.setChunked(true);
+                fill(data, resp, sent -> {
+                  cf.complete(null);
+                  resp.drainHandler(v -> {
+                    assertSameEventLoop(serverCtx, Vertx.currentContext());
+                    resp.end();
+                  });
+                });
+              });
+            }
+          });
+        });
         req.handler(chunk -> {
           assertSameEventLoop(serverCtx, Vertx.currentContext());
           body.appendBuffer(chunk);
-        });
-        req.endHandler(v2 -> {
-          assertSameEventLoop(serverCtx, Vertx.currentContext());
-          requests.add(req);
-          if (requests.size() == numReqs) {
-            requests.forEach(req_ ->{
-              HttpServerResponse resp = req_.response();
-              CompletableFuture<Void> cf = new CompletableFuture<>();
-              responseResumeMap.put(req_.path(), cf);
-              resp.setChunked(true);
-              fill(data, resp, () -> {
-                cf.complete(null);
-                resp.drainHandler(v -> {
-                  assertSameEventLoop(serverCtx, Vertx.currentContext());
-                  resp.end();
-                });
-              });
-            });
-          }
         });
       });
       server.listen(testAddress, onSuccess(s -> {
@@ -2131,13 +2137,14 @@ public class Http1xTest extends HttpTest {
     Set<Context> contexts = new ConcurrentHashSet<>();
     Set<Thread> threads = new ConcurrentHashSet<>();
     client.close();
+    client = null;
     Context clientCtx = vertx.getOrCreateContext();
     clientCtx.runOnContext(v -> {
       client = vertx.createHttpClient(createBaseClientOptions().setMaxPoolSize(numConns));
     });
     waitUntil(() -> client != null);
     for (int i = 0; i < numReqs; i++) {
-      CompletableFuture<Void> cf = new CompletableFuture<>();
+      CompletableFuture<Long> cf = new CompletableFuture<>();
       String path = "/" + i;
       requestResumeMap.put(path, cf);
       Context requestCtx = vertx.getOrCreateContext();
@@ -2168,9 +2175,7 @@ public class Http1xTest extends HttpTest {
         });
         req.sendHead(version -> {
           assertSameEventLoop(requestCtx, Vertx.currentContext());
-          fill(data, req, () -> {
-            cf.complete(null);
-          });
+          fill(data, req, cf::complete);
         });
       });
     }
@@ -3508,12 +3513,16 @@ public class Http1xTest extends HttpTest {
       client.close();
       client = vertx.createHttpClient(createBaseClientOptions().setMaxPoolSize(1).setPipelining(pipelined).setKeepAlive(true));
       HttpClientRequest req1 = client.request(HttpMethod.GET, testAddress, DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST, "/1", onFailure(err -> {
-        sendResponse.complete(null);
-      }));
-      if (pipelined) {
-        requestReceived.thenAccept(v -> {
-          req1.reset();
+        // We need a small delay before the server send the HTTP response
+        // so the stream is reset before the response is received by the client
+        vertx.setTimer(100, id -> {
+          sendResponse.complete(null);
         });
+      }));
+      requestReceived.thenAccept(v -> {
+        req1.reset();
+      });
+      if (pipelined) {
         AtomicInteger connCount = new AtomicInteger();
         client.connectionHandler(conn -> {
           if (connCount.getAndIncrement() == 0) {
@@ -3530,9 +3539,6 @@ public class Http1xTest extends HttpTest {
         });
         req1.end();
       } else {
-        requestReceived.thenAccept(v -> {
-          req1.reset();
-        });
         req1.end();
         client.request(HttpMethod.GET, testAddress, DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST, "/2", onSuccess(resp -> {
           assertEquals(200, resp.statusCode());
